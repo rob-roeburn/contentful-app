3b237b0e681235076d2369a3e98b0ba4a4dcdf0b
Rob Young <rob@roeburn.com>
2020-09-15 11:13:05 +0100

    Initial commit

---
+++ build/precache-manifest.da7c78c30a5e3eea9dabdf31d52c61c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "190c892c9318dbdaa03a3416b5ca7510",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a881b0b75112c33965cb",
+    "url": "/static/css/2.6ccd95d0.chunk.css"
+  },
+  {
+    "revision": "4aa3eb611b0b39934121",
+    "url": "/static/css/main.8ed833f1.chunk.css"
+  },
+  {
+    "revision": "a881b0b75112c33965cb",
+    "url": "/static/js/2.a1a4496e.chunk.js"
+  },
+  {
+    "revision": "b674ba54ed1da4a284a9f2dbd781a504",
+    "url": "/static/js/2.a1a4496e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4aa3eb611b0b39934121",
+    "url": "/static/js/main.bc2e1d51.chunk.js"
+  },
+  {
+    "revision": "f68a15ee2daa7632c690",
+    "url": "/static/js/runtime-main.bb6a70cc.js"
+  }
+]);
