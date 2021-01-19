bdbe39d673a270a7e9d307d0d77184fb448ecd04
Rob Young <rob@roeburn.com>
2021-01-19 11:44:59 +0000

    Add label to confirm env

--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -110,6 +110,7 @@ const Field = (props: FieldProps) => {
       ],
       handlers: {
         'image': React.useCallback(imageFunc => {
+          console.log("rich-editor-dev");
           props.sdk.dialogs.selectMultipleAssets()
           .then( (promiseData: any) => {
             if (typeof(promiseData) != 'undefined') {
